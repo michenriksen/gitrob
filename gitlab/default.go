42d6ba625174b3edea8329a586f3b78792fd3785
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-03-02 18:09:04 -0500

    WIP:  stubb in gitlab package exported method

---
+++ gitlab/default.go
@@ -0,0 +1,10 @@
+package gitlab
+
+import (
+	"github.com/codeEmitter/gitrob/common"
+	"github.com/xanzy/go-gitlab"
+)
+
+func GetUserOrOrganization(login string, client *gitlab.Client) (*common.Owner, error) {
+
+}
