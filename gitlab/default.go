2e84d3bc5adabdcff476bde2e52175a1002e8a2e
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-03-03 16:40:06 -0500

    return a single user and simplify

--- gitlab/default.go
+++ gitlab/default.go
@@ -5,34 +5,34 @@ import (
 	"github.com/xanzy/go-gitlab"
 )
 
-func getUser(login string, client *gitlab.Client) ([]*gitlab.User, error) {
+func getUser(login string, client *gitlab.Client) (*gitlab.User, error) {
 	user, _, err := client.Users.ListUsers(&gitlab.ListUsersOptions{Username: gitlab.String(login)})
 	if err != nil {
 		return nil, err
 	}
-	return user, err
+	return user[0], err
 }
 
 func GetUserOrOrganization(login string, client *gitlab.Client) (*common.Owner, error) {
-	users, err := getUser(login, client)
+	user, err := getUser(login, client)
 	if err != nil {
 		return nil, err
 	}
 
-	id := int64(users[0].ID)
+	id := int64(user.ID)
 	emptyString := gitlab.String("")
 
 	return &common.Owner{
-		Login:     gitlab.String(users[0].Username),
+		Login:     gitlab.String(user.Username),
 		ID:        &id,
 		Type:      gitlab.String("User"),
-		Name:      gitlab.String(users[0].Name),
-		AvatarURL: gitlab.String(users[0].AvatarURL),
-		URL:       gitlab.String(users[0].WebsiteURL),
-		Company:   gitlab.String(users[0].Organization),
+		Name:      gitlab.String(user.Name),
+		AvatarURL: gitlab.String(user.AvatarURL),
+		URL:       gitlab.String(user.WebsiteURL),
+		Company:   gitlab.String(user.Organization),
 		Blog:      emptyString,
 		Location:  emptyString,
-		Email:     gitlab.String(users[0].PublicEmail),
-		Bio:       gitlab.String(users[0].Bio),
+		Email:     gitlab.String(user.PublicEmail),
+		Bio:       gitlab.String(user.Bio),
 	}, nil
 }
