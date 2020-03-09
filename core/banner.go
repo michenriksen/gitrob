54f701e133b0613b3627f29bb2ca9f16aa69ed1f
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-03-09 18:03:05 -0400

    version bump

--- core/banner.go
+++ core/banner.go
@@ -1,13 +1,13 @@
 package core
 
 const (
-  Name        = "gitrob"
-  Version     = "2.0.0-beta"
-  Author      = "Michael Henriksen"
-  Website     = "https://github.com/michenriksen/gitrob"
-  ASCIIBanner = "        _ __           __\n" +
-    "  ___ _(_) /________  / /\n" +
-    " / _ `/ / __/ __/ _ \\/ _ \\\n" +
-    " \\_, /_/\\__/_/  \\___/_.__/\n" +
-    "/___/ by @michenriksen"
+	Name        = "gitrob"
+	Version     = "2.1.0-beta"
+	Author      = "Michael Henriksen"
+	Website     = "https://github.com/michenriksen/gitrob"
+	ASCIIBanner = "        _ __           __\n" +
+		"  ___ _(_) /________  / /\n" +
+		" / _ `/ / __/ __/ _ \\/ _ \\\n" +
+		" \\_, /_/\\__/_/  \\___/_.__/\n" +
+		"/___/ by @michenriksen"
 )
