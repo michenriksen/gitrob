ef403fecefe005582c86b1371612e8b24f50f4a1
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-03-23 17:30:12 -0400

    load signatures depending on mode

--- matching/signatures.go
+++ matching/signatures.go
@@ -13,16 +13,33 @@ type Signatures struct {
 	ContentSignatures []ContentSignature
 }
 
+func (s *Signatures) loadSignatures(path string) error {
+	if !common.FileExists(path) {
+		return errors.New(fmt.Sprintf("Missing signature file: %s.\n", path))
+	}
+	data, readError := ioutil.ReadFile(path)
+	if readError != nil {
+		return readError
+	}
+	if unmarshalError := json.Unmarshal(data, &s); unmarshalError != nil {
+		return unmarshalError
+	}
+	return nil
+}
+
 func (s *Signatures) Load(mode int) error {
-	fileSignaturePath := "./filesignatures.json"
-	if !common.FileExists(fileSignaturePath) {
-		return errors.New(fmt.Sprintf("Missing signature file: %s.\n", fileSignaturePath))
+	var e error
+	if mode != 3 {
+		e = s.loadSignatures("./filesignatures.json")
+		if e != nil {
+			return e
+		}
 	}
-	data, err := ioutil.ReadFile(fileSignaturePath); if err != nil {
-		return err
-	}
-	if err2 := json.Unmarshal(data, &s); err2 != nil {
-		return err2
+	if mode != 1 {
+		e = s.loadSignatures("./contentsignatures.json")
+		if e != nil {
+			return e
+		}
 	}
 	return nil
 }
