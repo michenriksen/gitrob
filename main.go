694b6532152bc195e644038fc48ae10db0ae360b
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-03-24 11:10:23 -0400

    output details of loaded signatures on launch

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 
 	sess.Out.Info("%s\n\n", common.ASCIIBanner)
 	sess.Out.Important("%s v%s started at %s\n", common.Name, common.Version, sess.Stats.StartedAt.Format(time.RFC3339))
-	sess.Out.Important("Loaded %d signatures\n", len(sess.Signatures.FileSignatures) + len(sess.Signatures.ContentSignatures))
+	sess.Out.Important("Loaded %d file signatures and %d content signatures.\n", len(sess.Signatures.FileSignatures), len(sess.Signatures.ContentSignatures))
 	sess.Out.Important("Web interface available at http://%s:%d\n", *sess.Options.BindAddress, *sess.Options.Port)
 
 	if sess.Stats.Status == "finished" {
