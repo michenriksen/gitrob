a44e1400d77474b697490379465e28d1bba2ba3e
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-03-27 13:12:00 -0400

    add in-mem support for github

--- github/git.go
+++ github/git.go
@@ -2,6 +2,7 @@ package github
 
 import (
 	"fmt"
+	"gopkg.in/src-d/go-git.v4/storage/memory"
 	"io/ioutil"
 
 	"github.com/codeEmitter/gitrob/common"
@@ -11,17 +12,27 @@ import (
 )
 
 func CloneRepository(cloneConfig *common.CloneConfiguration) (*git.Repository, string, error) {
-	dir, err := ioutil.TempDir("", "gitrob")
-	if err != nil {
-		return nil, "", err
-	}
-	repository, err := git.PlainClone(dir, false, &git.CloneOptions{
+
+	cloneOptions := &git.CloneOptions{
 		URL:           *cloneConfig.Url,
 		Depth:         *cloneConfig.Depth,
 		ReferenceName: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", *cloneConfig.Branch)),
 		SingleBranch:  true,
 		Tags:          git.NoTags,
-	})
+	}
+
+	var repository *git.Repository
+	var err error
+	var dir string
+	if !*cloneConfig.InMemClone {
+		dir, err := ioutil.TempDir("", "gitrob")
+		if err != nil {
+			return nil, "", err
+		}
+		repository, err = git.PlainClone(dir, false, cloneOptions)
+	} else {
+		repository, err = git.Clone(memory.NewStorage(), nil, cloneOptions)
+	}
 	if err != nil {
 		return nil, dir, err
 	}
