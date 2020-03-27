9c3a9e64de568a963e3eee055d104fa9313998c1
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-03-27 16:50:49 -0400

    remove unused vars

--- common/banner.go
+++ common/banner.go
@@ -3,8 +3,6 @@ package common
 const (
 	Name        = "gitrob"
 	Version     = "2.1.0-beta"
-	Author      = "Michael Henriksen"
-	Website     = "https://github.com/michenriksen/gitrob"
 	ASCIIBanner = "        _ __           __\n" +
 		"  ___ _(_) /________  / /\n" +
 		" / _ `/ / __/ __/ _ \\/ _ \\\n" +
