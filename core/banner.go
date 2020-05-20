06beaf2d738810f67ee9e4b12770bb991a0bdb04
Matt Jones <urlugal@gmail.com>
2020-05-20 00:29:47 -0400

    Releasing v

--- core/banner.go
+++ core/banner.go
@@ -1,13 +1,12 @@
 package core
 
 const (
-  Name        = "gitrob"
-  Version     = "2.0.0-beta"
-  Author      = "Michael Henriksen"
-  Website     = "https://github.com/michenriksen/gitrob"
-  ASCIIBanner = "        _ __           __\n" +
-    "  ___ _(_) /________  / /\n" +
-    " / _ `/ / __/ __/ _ \\/ _ \\\n" +
-    " \\_, /_/\\__/_/  \\___/_.__/\n" +
-    "/___/ by @michenriksen"
+	Name        = "gitrob"
+	Version     = "2.0.0-rc1"
+	Website     = "https://github.com/mattyjones/gitrob"
+	ASCIIBanner = "        _ __           __\n" +
+		"  ___ _(_) /________  / /\n" +
+		" / _ `/ / __/ __/ _ \\/ _ \\\n" +
+		" \\_, /_/\\__/_/  \\___/_.__/\n" +
+		"/___/ by @michenriksen"
 )
